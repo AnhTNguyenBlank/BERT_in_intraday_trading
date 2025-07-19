a521b6ee6e794a6a6fc37ed0e53aabfb25e1b962
Blanky <tuananhnguyen.moi@gmail.com>
2025-07-19 10:05:27 +0700

    Add preprocessing news_data (labelling and export as df in pickle)

--- Training/Workplace/prepare_data.py
+++ Training/Workplace/prepare_data.py
@@ -25,6 +25,8 @@ import pickle
 
 if __name__ == '__main__':
 
+    #=========================== Scrape source for news_data ===========================#
+
     # driver = set_up_driver(num_clicks = 1000, time_sleep_open = 5)
 
     # # After loading all articles, parse page source with BeautifulSoup
@@ -35,6 +37,7 @@ if __name__ == '__main__':
     # with open('D:/BERT_in_intraday_trading/Training/Saved_results/page_source.html', 'w', encoding='utf-8') as f:
     #     f.write(str(soup))
 
+    #=========================== Extract news_data from source ===========================#
 
     # Reload from file
     # with open('D:/BERT_in_intraday_trading/Training/Saved_results/page_source.html', 'r', encoding='utf-8') as f:
@@ -50,7 +53,7 @@ if __name__ == '__main__':
     #     if link.string and len(link.string.strip()) > 35 and link.has_attr('href')
     # ]
 
-    # for link in tqdm(valid_links[6000:10000], desc="Extracting articles' contents", unit = 'article'):
+    # for link in tqdm(valid_links[10000:], desc="Extracting articles' contents", unit = 'article'):
     #     full_url = link['href']
     #     time_posted, content = extract_article_content(full_url)
     #     date_posted = link['href'][-10:]
@@ -67,13 +70,51 @@ if __name__ == '__main__':
     #     stored_data.append(temp)
 
 
-    # with open("D:/BERT_in_intraday_trading/Training/Data/stored_data_3.pkl", "wb") as f:
+    # with open("D:/BERT_in_intraday_trading/Training/Data/stored_data.pkl", "wb") as f:
     #     pickle.dump(stored_data, f)
 
     
-    # Load the pickle file
-    
-    with open("D:/BERT_in_intraday_trading/Training/Data/stored_data.pkl", "rb") as f:
-        my_loaded_array = pickle.load(f)
+    #=========================== Load news_data ===========================#
 
-    print(my_loaded_array)
+    with open("D:/BERT_in_intraday_trading/Training/Data/stored_data.pkl", "rb") as f:
+        news_data = pickle.load(f)
+        
+    news_data = [new for new in news_data if new['CONTENT'] != 'content']
+    news_data = pd.DataFrame(news_data)
+    news_data.set_index(keys = 'TIME_POSTED', inplace = True)
+    news_data.index = pd.to_datetime(news_data.index).tz_localize(None)
+    news_data = news_data[~news_data.index.isna()].sort_index()
+
+    #=========================== Load price_data ===========================#
+
+    interval_data = pd.read_pickle('D:/BERT_in_intraday_trading/Training/Data/XAUUSDm_M1.pkl')
+
+    interval_data = interval_data.set_index('DATE_TIME')
+    interval_data.index = pd.to_datetime(interval_data.index)
+
+    interval_data['DATE'] = pd.to_datetime(interval_data['DATE'])
+    interval_data['OPEN'] = interval_data['OPEN']
+    interval_data['HIGH'] = interval_data['HIGH']
+    interval_data['LOW'] = interval_data['LOW']
+    interval_data['CLOSE'] = interval_data['CLOSE']
+
+    df_1_min = prepare_df(df = interval_data, timeframe = '1min', add_indicators = True)
+
+
+    #=========================== Labelling ===========================#
+
+    news_data['MEAN_BA'] = news_data.apply(lambda x: df_1_min.loc[(df_1_min.index >= x.name - pd.Timedelta(hours = 4)) & (df_1_min.index <= x.name + pd.Timedelta(hours = 4)), 'Ret(t)'].mean(), axis = 1)
+    news_data['VAR_BA'] = news_data.apply(lambda x: df_1_min.loc[(df_1_min.index >= x.name - pd.Timedelta(hours = 4)) & (df_1_min.index <= x.name + pd.Timedelta(hours = 4)), 'Ret(t)'].var(), axis = 1)
+
+    news_data['MEAN_B'] = news_data.apply(lambda x: df_1_min.loc[(df_1_min.index >= x.name - pd.Timedelta(hours = 4)) & (df_1_min.index <= x.name), 'Ret(t)'].mean(), axis = 1)
+    news_data['VAR_B'] = news_data.apply(lambda x: df_1_min.loc[(df_1_min.index >= x.name - pd.Timedelta(hours = 4)) & (df_1_min.index <= x.name), 'Ret(t)'].var(), axis = 1)
+
+    news_data['MEAN_A'] = news_data.apply(lambda x: df_1_min.loc[(df_1_min.index >= x.name) & (df_1_min.index <= x.name + pd.Timedelta(hours = 4)), 'Ret(t)'].mean(), axis = 1)
+    news_data['VAR_A'] = news_data.apply(lambda x: df_1_min.loc[(df_1_min.index >= x.name) & (df_1_min.index <= x.name + pd.Timedelta(hours = 4)), 'Ret(t)'].var(), axis = 1)
+
+    news_data['RATIO_VAR_A_B'] = news_data['VAR_A']/news_data['VAR_B']
+    news_data['RATIO_MEAN_A_B'] = news_data['MEAN_A']/news_data['MEAN_B']
+    news_data['FLAG_HIGH_RISK'] = news_data['RATIO_VAR_A_B'].apply(lambda x: 1 if x >= news_data['RATIO_VAR_A_B'].quantile(0.75) else 0)
+
+
+    news_data.to_pickle('D:/BERT_in_intraday_trading/Training/Data/news_data_w_labels.pkl')
