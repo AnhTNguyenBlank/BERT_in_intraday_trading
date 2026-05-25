b52f8ab79728fbf6a1248cf36bc898055c93bf84
Blanky <tuananhnguyen.moi@gmail.com>
2026-05-24 20:20:05 -0400

    Add deepLLM ARCH (remove the RV)

--- src/models.py
+++ src/models.py
@@ -1,11 +1,7 @@
 import pandas as pd
 import numpy as np
 
-pd.set_option('display.max_columns', 999)
-
-import ta
-import os
-import math
+# pd.set_option('display.max_columns', 999)
 
 import matplotlib.pyplot as plt
 
@@ -13,16 +9,8 @@ from src.support import *
 from src.backtest import *
 
 import tensorflow as tf
-import tensorflow_hub as hub
 import keras
 import keras_hub
-from abc import ABC, abstractmethod
-import keras
-from keras import layers
-import pickle
-
-import lightgbm as lgb
-from sklearn.metrics import mean_pinball_loss
 
 import warnings
 warnings.filterwarnings('ignore')
@@ -34,6 +22,8 @@ class DeepARCH(tf.keras.Model):
     def __init__(self, lstm_units=20):
         super().__init__()
 
+        self.BatchNorm = tf.keras.layers.BatchNormalization()
+        self.Dropout = tf.keras.layers.Dropout(0.3)
         self.lstm = tf.keras.layers.LSTM(lstm_units)
         self.dense1 = tf.keras.layers.Dense(20, activation='relu')
         self.dense2 = tf.keras.layers.Dense(1)
@@ -45,7 +35,9 @@ class DeepARCH(tf.keras.Model):
         return [self.loss_tracker]
 
     def call(self, x, training=False):
+        x = self.BatchNorm(x, training=training)
         h = self.lstm(x, training=training)
+        h = self.Dropout(h, training=training)
         h = self.dense1(h)
         log_sigma2 = self.dense2(h)
         return log_sigma2
@@ -83,11 +75,13 @@ class DeepRARCH(tf.keras.Model):
     def __init__(self, lstm_units=20):
         super().__init__()
 
+        self.BatchNorm = tf.keras.layers.BatchNormalization()
+        self.Dropout = tf.keras.layers.Dropout(0.3)
         self.lstm = tf.keras.layers.LSTM(lstm_units)
         self.dense1 = tf.keras.layers.Dense(20, activation='relu')
         self.dense2 = tf.keras.layers.Dense(1)
         self.dense3 = tf.keras.layers.Dense(1, activation='softplus')
-        self.log_sigma2_u = tf.Variable(0.0, trainable=True, dtype=tf.float32)
+        self.log_sigma2_u = tf.Variable(-2.4, trainable=True, dtype=tf.float32)
         self.loss_tracker = tf.keras.metrics.Mean(name="loss")
 
     @property
@@ -98,8 +92,10 @@ class DeepRARCH(tf.keras.Model):
         
         x, rv = inputs
         inputs = tf.concat([x, rv], axis=2)
+        inputs = self.BatchNorm(inputs, training=training)
 
         h = self.lstm(inputs, training=training)
+        h = self.Dropout(h, training=training)
         h = self.dense1(h)
         log_sigma2 = self.dense2(h)
         rv_hat = self.dense3(h)
@@ -152,21 +148,25 @@ class DeepLLMRARCH(tf.keras.Model):
     def __init__(self, lstm_units=20):
         super().__init__()
 
+        self.BatchNorm = tf.keras.layers.BatchNormalization()
+        self.Dropout = tf.keras.layers.Dropout(0.3)
+        self.text_dropout = tf.keras.layers.Dropout(0.1)
+
         self.lstm = tf.keras.layers.LSTM(lstm_units)
 
         self.dense1 = tf.keras.layers.Dense(20, activation='relu')
         self.dense2 = tf.keras.layers.Dense(1)
         self.dense3 = tf.keras.layers.Dense(1, activation='softplus')
-        self.text_projection = tf.keras.layers.Dense(16, activation="relu")
+        self.text_projection = tf.keras.layers.Dense(64, activation="relu")
 
-        self.log_sigma2_u = tf.Variable(0.0, trainable=True, dtype=tf.float32)
+        self.log_sigma2_u = tf.Variable(-2.4, trainable=True, dtype=tf.float32)
 
         # Text encoder
         self.text_model = self._get_text_model()
 
         # Enable LoRA
-        self.text_model.quantize("int8")
-        self.text_model.backbone.enable_lora(rank=4)
+        # self.text_model.quantize("int8")
+        self.text_model.backbone.enable_lora(rank=8)
 
         self.loss_tracker = tf.keras.metrics.Mean(name="loss")
 
@@ -195,11 +195,13 @@ class DeepLLMRARCH(tf.keras.Model):
 
         text_vec = self.text_model(text, training=training)      # (batch, 768)
         text_vec = self.text_projection(text_vec)  # (batch, 32)
+        text_vec = self.text_dropout(text_vec, training=training)
 
         ts_inputs = tf.concat([x, rv], axis=2)
+        ts_inputs = self.BatchNorm(ts_inputs, training=training)
 
         h = self.lstm(ts_inputs, training=training)
-
+        h = self.Dropout(h, training=training)
         h = tf.concat([h, text_vec], axis=1)
 
         h = self.dense1(h)
@@ -212,7 +214,7 @@ class DeepLLMRARCH(tf.keras.Model):
     def train_step(self, data):
 
         inputs, targets = data
-        r_t, rv_t = targets
+        r_t, rv_t   = targets
 
         with tf.GradientTape() as tape:
 
@@ -238,8 +240,7 @@ class DeepLLMRARCH(tf.keras.Model):
     def test_step(self, data):
 
         inputs, targets = data
-        r_t, rv_t = targets
-
+        r_t, rv_t   = targets['r_target'], targets['rv_target']
         log_sigma2, rv_hat, log_sigma2_u = self(inputs, training=False)
 
         sigma2 = tf.exp(log_sigma2) + 1e-6
@@ -256,3 +257,77 @@ class DeepLLMRARCH(tf.keras.Model):
 
         return {"loss": self.loss_tracker.result()}
 
+class DeepLLMARCH(tf.keras.Model):
+
+    def __init__(self, lstm_units=20):
+        super().__init__()
+
+        self.BatchNorm = tf.keras.layers.BatchNormalization()
+        self.Dropout = tf.keras.layers.Dropout(0.3)
+        self.text_dropout = tf.keras.layers.Dropout(0.1)
+
+        self.lstm            = tf.keras.layers.LSTM(lstm_units)
+        self.dense1          = tf.keras.layers.Dense(20, activation='relu')
+        self.dense2          = tf.keras.layers.Dense(1)
+        self.text_projection = tf.keras.layers.Dense(64, activation="relu")
+
+        self.text_model = self._get_text_model()
+        self.text_model.backbone.enable_lora(rank=8)
+
+        self.loss_tracker = tf.keras.metrics.Mean(name="loss")
+
+    @property
+    def metrics(self):
+        return [self.loss_tracker]
+
+    def _get_text_model(self):
+        text_input   = keras.Input(shape=(), dtype="string")
+        preprocessor = keras_hub.models.TextClassifierPreprocessor.from_preset("bert_tiny_en_uncased")
+        backbone     = keras_hub.models.Backbone.from_preset("bert_tiny_en_uncased")
+
+        x = preprocessor(text_input)
+        x = backbone(x)["pooled_output"]
+
+        model         = keras.Model(text_input, x)
+        model.backbone = backbone
+        return model
+
+    def call(self, inputs, training=False):
+        x, text = inputs
+
+        text_vec = self.text_model(text, training=training)  
+        text_vec = self.text_projection(text_vec)      
+        text_vec = self.text_dropout(text_vec, training=training)
+
+        ts_inputs = x
+        ts_inputs = self.BatchNorm(ts_inputs, training=training)
+        h         = self.lstm(ts_inputs, training=training)
+        h = self.Dropout(h, training=training)
+        h         = tf.concat([h, text_vec], axis=1)
+        h         = self.dense1(h)
+
+        log_sigma2 = self.dense2(h)
+        return log_sigma2
+
+    def train_step(self, data):
+        inputs, r_t = data
+
+        with tf.GradientTape() as tape:
+            log_sigma2 = self(inputs, training=True)
+            sigma2     = tf.exp(log_sigma2) + 1e-6
+            loss       = tf.reduce_mean(log_sigma2 + (r_t ** 2) / sigma2)
+
+        grads = tape.gradient(loss, self.trainable_weights)
+        self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
+        self.loss_tracker.update_state(loss)
+        return {"loss": self.loss_tracker.result()}
+
+    def test_step(self, data):
+        inputs, r_t = data
+
+        log_sigma2 = self(inputs, training=False)
+        sigma2     = tf.exp(log_sigma2) + 1e-6
+        loss       = tf.reduce_mean(log_sigma2 + (r_t ** 2) / sigma2)
+
+        self.loss_tracker.update_state(loss)
+        return {"loss": self.loss_tracker.result()}
